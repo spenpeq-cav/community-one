0ed754955bd2872e6a0ab2c7a123c45ac1caff76
Spencer Pequegnat <spencer.peq@gmail.com>
2021-11-07 16:22:05 -0800

    Like query, Likes from backend with test Card component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,29 +2,51 @@ import './App.css';
 import { ApolloProvider, gql, useQuery } from "@apollo/client";
 import { client } from "./apollo/index";
 
-const get_clients_query = gql`
+const get_projects_query = gql`
   query {
-    clients {
+    projects {
       id
-      firstName
-      lastName
+      name
+      description
+      developer {
+        id
+        firstName
+        lastName
+      }
+      githubURL
+      livesiteURL
+      likeCount
+      published_at
     }
   }
 `
+const get_likes_by_project_id= gql`
+query GetLikesByPortfolioId($projectID: ID!) {
+  likes(where: { project: { id: $projectID } }) {
+    id
+  }
+}
+`
 
-function ClientList() {
-  const {error, data, loading} = useQuery(get_clients_query)
+function ProjectList() {
+  const {error, data, loading} = useQuery(get_projects_query)
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error</p>;
 
   console.log(data)
-  return <div>
-    <h1>Client List</h1>
-    {data && <div>
-      {data.clients.map(client => <h1 key={client.id}>{client.firstName} {client.lastName}</h1>)}
-    </div>}
-  </div>
+  return (
+    <div>
+      <h1>Projects List</h1>
+      { data && 
+        <div>
+          { data.projects.map((project) => 
+            <Card key={project.id} project={project}></Card>
+          )}
+        </div>
+      }
+    </div>
+  )
 }
 
 function App() {
@@ -32,9 +54,35 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <div className="App">
-        <ClientList />
+        <ProjectList />
       </div>  
     </ApolloProvider>
+  );
+}
+
+function Card({project}) {
+  
+  const { firstName, lastName } = project.developer
+
+  function handleLike (){
+    console.log("Liked")
+  }
+
+  const {error, data, loading} = useQuery(get_likes_by_project_id, {
+    variables: { projectID: project.id}
+  })
+  
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error</p>;
+  console.log(data)
+  return (
+    <div key={project.id}>
+      <h1>{project.name}</h1>
+      <p>{project.description}</p>
+      <p>{`${firstName} ${lastName}`}</p>
+      <span>Likes: {data.likes.length}</span>
+      <button onClick={handleLike}>Like</button>
+    </div>
   );
 }
 
